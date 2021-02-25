0728cbf4f7ecde0012bddd84d9000de3f6fb0752
n-desantis92 <dsnwebdev@gmail.com>
2021-02-25 16:12:01 +0100

    domande e stampa

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var nome = prompt("qualè il tuo nome?");
+
+var cognome = prompt("qualè il tuo cognome?");
+
+var colore = prompt("colore preferito?");
+
+document.getElementById('risultato').innerHTML = (nome + cognome + colore + 21);
